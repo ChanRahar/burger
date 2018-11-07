c2090d0016d60c9b73ecfa06fe622e0aa7561b42
ChanR <chan.rahar@gmail.com>
2018-11-07 18:11:23 -0500

    added all necessary files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Double Bacon Cheeseburger', false),
+('Triple-Stacked Avocado Burger', false),
+('Cali-Chicken Cheeseburger', false),
+('Southern Style Veggie Burger', true),
+('Spicy Pepper Jack Mushroom Burger', true),
+('Tequila Lime Burger', true);
